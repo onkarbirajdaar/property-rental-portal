6551de7b529def32a3f44037b3107186041dd698
onkarbirajdaar <onkarbirajdar098@gmail.com>
2026-07-30 11:53:11 +0530

    Set WHITENOISE_MANIFEST_STRICT to handle missing cloudinary static asset

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,7 @@ STORAGES = {
     },
 }
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
