1ee8c1b5f25a96cb299a11bc8e1b577300a1abc7
onkarbirajdaar <onkarbirajdar098@gmail.com>
2026-08-04 16:51:22 +0530

    Remove cloudinary_storage from INSTALLED_APPS - its collectstatic override was silently skipping all static files

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
+    
     'django.contrib.staticfiles',
     'cloudinary',
     "accounts",
