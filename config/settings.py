3e7b27a10a2e6a04249e3af699f635420bc89940
onkarbirajdaar <onkarbirajdar098@gmail.com>
2026-08-31 09:02:27 +0530

    Switch database to SQLite and ignore generated files

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     'django.contrib.staticfiles',
     'cloudinary',
+    'cloudinary_storage',
     "accounts",
     "properties",
 ]
@@ -79,10 +80,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-'default': dj_database_url.config(
-    default=config('DATABASE_URL', default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
-    conn_max_age=600,
-)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
