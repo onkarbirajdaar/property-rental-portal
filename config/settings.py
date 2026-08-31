bd64b48d24dd502d54c52a6e21164bcf35d26911
onkarbirajdaar <onkarbirajdar098@gmail.com>
2026-08-31 10:38:53 +0530

    fix: add PythonAnywhere host

--- config/settings.py
+++ config/settings.py
@@ -26,8 +26,11 @@ SECRET_KEY =config('SECRET_KEY')
 DEBUG =config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS =config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
-
+ALLOWED_HOSTS = [
+    'onkarbirajdar.pythonanywhere.com',
+    '127.0.0.1',
+    'localhost',
+]
 
 # Application definition
 
